a363cefed3b2495b641ea4f3d19afc5c1645b98c
Ne184 <64256244+Ne184@users.noreply.github.com>
2023-05-08 19:07:49 +0200

    Update predicting.py

--- model_training/predicting.py
+++ model_training/predicting.py
@@ -15,7 +15,7 @@ def predict_fresh_X(classifier, cv):
     dataset.to_csv("assets/c3_Predicted_Sentiments_Fresh_Dump.tsv", sep='\t', encoding='UTF-8', index=False)
 
 
-def predict_single(classifier, review, cv):
+def predict_single(classifier, review, cv) -> float:
     processed_input = cv.transform([review]).toarray()[0]
     prediction = classifier.predict([processed_input])[0]
 
@@ -24,6 +24,16 @@ def predict_single(classifier, review, cv):
         1: "positive"
     }
     print(f"The model believes the review is {prediction_map[prediction]}.")
+    return prediction
+
+
+def predict_single_review(review: str) -> float:
+    cvFile = 'c1_BoW_Sentiment_Model.pkl'
+    cv = pickle.load(open(cvFile, "rb"))
+
+    # Predictions (via sentiment classifier)
+    classifier = joblib.load('c2_Classifier_Sentiment_Model')
+    return predict_single(classifier, review, cv)
 
 
 def main():
