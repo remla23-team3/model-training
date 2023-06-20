59e014b8a06bfa894799d1c144f1b988ef9a267b
Ne184 <64256244+Ne184@users.noreply.github.com>
2023-06-20 13:54:42 +0200

    remove src form the import

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,7 +4,7 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from src.data.preprocess import load_dataset
+from data.preprocess import load_dataset
 
 def train_model(X_train, y_train):
     """
